bfdccaaf801cbf30829d60b106fee57d0122cd97
ttxtpy <22001960@siswa.um.edu.my>
2024-02-24 00:33:15 +0800

    mmm lopa

---
+++ study.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("butang");
+const gambar = document.getElementById("lopaTidur");
+
+button.addEventListener("click",ditekan =>{
+    gambar.src = "bangun.jpg";
+});
