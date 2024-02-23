c422ef434259cdffb35c4fe1869fc715ba6a3055
ttxtpy <22001960@siswa.um.edu.my>
2024-02-24 01:15:28 +0800

    add lines of lopa

--- study.js
+++ study.js
@@ -1,6 +1,14 @@
 const button = document.getElementById("butang");
 const gambar = document.getElementById("lopaTidur");
 
-button.addEventListener("click",ditekan =>{
+const userTekan = document.createElement("h1");
+//set attribute
+userTekan.textContent = "Lopa: You fool, I was eating my wet food in my dream, now you shall pay the price for waking me up!";
+userTekan.id = "tertekan";
+userTekan.style.color = "red";
+
+button.addEventListener("click",function(){
+    
     gambar.src = "bangun.jpg";
+    document.getElementsByClassName('isi')[0].insertBefore(userTekan,button);
 });
